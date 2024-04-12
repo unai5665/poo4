dd795eabfc144bd05a9863cb5042449aea7034ae
unai5665 <unaiperez5665@gmail.com>
2024-04-12 12:24:22 +0200

    añadidas excepciones

--- except1.py
+++ except1.py
@@ -13,9 +13,15 @@ def intDiv(a:int, b:int)->int:
 
 #Si es el flujo principal
 if __name__ == "__main__":
-    '''
+    
     for i in range(30):
         a = random.randint(0,9)
         b = random.randint(0,9)
         print("a:", a, "b:", b, intDiv(random.randint(0,9), 5))
-    '''   
+    
+    intDiv(2, "3")  
+
+    try:
+        intDiv()
+    except:
+        print("No lo consiguío")
