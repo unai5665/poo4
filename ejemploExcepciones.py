56daa4dfd94c73c33fc6d324502920dac5033d9d
unai5665 <unaiperez5665@gmail.com>
2024-04-17 09:31:48 +0200

    creado test_ejemploExcepciones

--- ejemploExcepciones.py
+++ ejemploExcepciones.py
@@ -1,7 +1,11 @@
-class EjemploExceptiones:
+class EjemploExcepciones:
     # ZeroDivisionError
+    def ZeroDivisionError(self, *, num:int, den:int)->int
+        if (b == 0)
 
     #ValueError
+    def ValueError(numero1:int=("Hola")):
+        print(numero1)
 
     #FileNotFoundError
 
