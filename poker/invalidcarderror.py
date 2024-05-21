74fb8a8318e8b4ac3906c2bf609b932c88712b3b
unai5665 <151141045+unai5665@users.noreply.github.com>
2024-05-21 20:23:30 +0100

    Delete poker/invalidcarderror.py

--- poker/invalidcarderror.py
+++
@@ -1,4 +0,0 @@
-class Invalidcarderror:
-    def __init__(self, suit:str, value:str )-> None:
-        self.suit = "CLUBS", "DIAMONDS", "HEARTS", "SPADES" 
-        self.value = {"A":1, "2":"2", "3":"3", "4":"4", "5":"5", "6":"6", "7":"7", "8":"8", "9":"9", "10":"10", "J":"11", "Q":"12", "K":"13"}
